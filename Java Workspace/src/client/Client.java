ec41e25a232a8df8a97ffa9489c6d524df6be4c7
lucas-fuda-46029540 <66055448+lucas-fuda@users.noreply.github.com>
2020-08-07 23:32:53 +1000

    Added code to populate a 255x255 array with RGB data matching the image

--- Java Workspace/src/client/Client.java
+++ Java Workspace/src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -11,7 +12,11 @@ public class Client {
 	// Instance Variables
 	public static BufferedImage image;
 	public static int[] res = new int[2];
-	public static int[][] imgArray  = new int[255][255];
+	public static Color[][] colorArray = new Color[255][255];
+	public static Color c;
+	public static int red;
+	public static int green;
+	public static int blue;
 	
 	// Getters
 	public static BufferedImage getImage() {
@@ -35,6 +40,18 @@ public class Client {
 		res[1] = image.getHeight();
 	}
 	
+	public static void colorArray() {
+		for (int i = 0; i < 255; i++) {
+			for (int j = 0; j < 255; j++) {
+				int rgb = getImage().getRGB(i, j);
+				Color c = new Color (rgb);
+				red = c.getRed();
+				green = c.getGreen();
+				blue = c.getBlue();
+				colorArray[i][j] = c;
+			}
+		}
+	}
 	// Constructors
 	
 	// Instance Methods 
@@ -47,9 +64,10 @@ public class Client {
 		g2d.dispose();
 		image = dimg;
 	}
-	public static void printRes() {
+	public static void printRes() { // print resolution
 		System.out.println(res[0] + "x" + res[1]);
 	}
+	
 	
 	// Void Main
 	public static void main(String[] args) {
@@ -59,9 +77,8 @@ public class Client {
 		resizeImage();
 		setRes(); 
 		printRes(); // methods exist just to make sure resizing has happened
-		
-		
-		
+		colorArray();
+		System.out.println(colorArray[221][241]);
 		
 	}
 
